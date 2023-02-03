c94d7cfcc00f43ed8d4f3cb90cec7ffa7c0ce547
marc Alimany <marc.alimany@enti.cat>
2023-02-03 11:22:12 +0100

    Ver bases de datos desde un puerto en un navegador web

--- index.js
+++ index.js
@@ -2,11 +2,38 @@
 
 //#!/usr/bin/env node
 
-
 const  http = require('http');
+const { MongoClient } = require('mongodb');
 
-http.createServer(function(request, result){
+// Connection URL
+const url = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'abascal';
+
+let db;
+async function db_connect() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  db = client.db(dbName);
+  //const collection = db.collection('documents');
+
+  return 'Conectados a la base de datos MongoDB';
+}
+
+db_connect()
+  .then(info => console.log(info))
+  .catch(msg => console.error(msg));
+
+
+let http_server = http.createServer(function(request, response){
+ let collection = db.collection('characters');
+ console.log (collection);		
  console.log("se conecta");
- result.write('ola k ase');
- result.end();
-}).listen(8080);
+ response.write('ola k ase');
+ response.end();
+})
+http_server.listen(8080);
+
