ea7d1695f32d113ef5575791e6c98ea4a799dac9
marc Alimany <marc.alimany@enti.cat>
2023-02-10 13:40:33 +0100

    html incluido

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 const  http = require('http');
 const { MongoClient } = require('mongodb');
+const fs = require('fs');
 
 // Connection URL
 const url = 'mongodb://127.0.0.1:27017';
@@ -27,13 +28,75 @@ db_connect()
   .then(info => console.log(info))
   .catch(msg => console.error(msg));
 
+function send_characters (response)
+{
+ let collection = db.collection('characters');
+ collection.find({}).toArray().then(characters => {
+   let names = [];
+   
+   for (let i = 0; i < characters.length; i++){
+      names.push( characters[i].name );
+}
+   
+   response.write(JSON.stringify(names));
+   response.end()
+});
+
+
+}
+function send_age (response, url){
+     
+  if (url.length < 3){
+      response.write("ERROR: Introduce un personaje");
+      response.end;
+      return;
+	}
+
+ let collection = db.collection('characters');
+ console.log(url);
+ collection.find({"name":url[2] }).project({_id:0,age:1})
+         .toArray().then(character => {
+	console.log(character);
+	 if (character.length == 0){
+       response.write("ERROR: Edad Erronea");
+			 response.end();
+			 return;
+	 }
+   response.write(JSON.stringify(character[0]));
+   response.end()
+   })
+}
 
 let http_server = http.createServer(function(request, response){
- let collection = db.collection('characters');
- console.log (collection);		
- console.log("se conecta");
- response.write('ola k ase');
- response.end();
-})
+  if (request.url == "/favicon.ico"){
+      return;
+}
+  let url = request .url.split("/");
+  
+  switch (url[1]){
+     case "characters":
+         send_characters(response);
+         break;
+     case "age":
+          send_age(response, url);
+          break;
+     default:
+					fs.readFile("index.html", function(err, data){
+					  if (err){
+								console.error(err);
+								response.writeHead(404, {'Content-Type':'text/html'});
+								response.write("Error 404: el archivo no esta en este castillo");
+
+								response.end();
+
+								return;
+						}
+						response.writeHead(200, {'Content-Type':'text/html'})
+            response.write(data);
+  			  	response.end()
+			});
+  	}        
+});
+
 http_server.listen(8080);
 
